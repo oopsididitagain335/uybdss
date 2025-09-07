402c3d0b93ca8d75f36d037eb9b5c17e265edd4c
Solace <monkdev9@gmail.com>
2025-09-07 03:06:01 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,13 +5,18 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public'), {
-  etag: false,
-  lastModified: false,
+  index: false,
   cacheControl: false
 }));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'), err => {
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/:name', (req, res) => {
+  const file = req.params.name.includes('.') ? req.params.name : req.params.name + '.html';
+  const filePath = path.join(__dirname, 'public', file);
+  res.sendFile(filePath, err => {
     if (err) res.status(404).send('Not Found');
   });
 });
